7f8571579b94886053c7a0f072bab416ad8c677c
EddieStn <eduard.eduard25@yahoo.com>
2022-08-26 06:00:36 +0000

    Create function to check player's guess

--- run.py
+++ run.py
@@ -6,61 +6,92 @@ def get_word():
     """
     Get a random word from the word list
     """
-    word = random.choice(word_list)
-    return word.upper()
+    random_word = random.choice(word_list)
+    return random_word.upper()
 
-def display_hangman(attempts):
-    if attempts == 0:
+
+word = get_word()
+print(word)
+
+
+
+def display_hangman(attempt):
+    """
+    Display the hangman building up on wrong choices
+    """
+    if attempt == 0:
         print("""
             +---+
             |
             |
             |
             ______""")
-    elif attempts == 1:
+    elif attempt == 1:
         print("""
             +---+
             |   O
             |
             |
             ______""")
-    elif attempts == 2:
+    elif attempt == 2:
         print("""
             +---+
             |   O
             |   |
             |
             ______""")    
-    elif attempts == 3:
+    elif attempt == 3:
         print("""
             +---+
             |   O
             |  /|
             |
             ______""") 
-    elif attempts == 4:
+    elif attempt == 4:
         print("""
             +---+
             |   O
             |  /|\\
             |
             ______""") 
-    elif attempts == 5:
+    elif attempt == 5:
         print("""
             +---+
             |   O
             |  /|\\
             |  /
             ______""") 
-    elif attempts == 6:
+    elif attempt == 6:
         print("""
             +---+
             |   O
             |  /|\\
             |  / \\
             ______""") 
+    return attempt
 
 
-display_hangman(6)
+attempt = display_hangman(0)
 
+
+while attempt <= 6:
+    def check_guess():
+        """
+        Check whether player`s input is a letter or not
+        """
+        global attempt
+        guess = input("Guess a letter: ")
+        for letter in guess:
+            if letter.isalpha():
+                print(f"You picked: {guess}")
+            else:
+                print("Not a letter")
+        if guess in word:
+            print(f"That is correct, {guess} is in the word")
+        else:
+            attempt += 1
+            print(f"{guess} is not in the word. attempts left: {attempt}")
+            
+
+check_guess()
     
