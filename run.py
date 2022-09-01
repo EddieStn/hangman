7731a29b933d11111614187b5b242e4d5a7242d0
EddieStn <eduard.eduard25@yahoo.com>
2022-09-01 18:32:11 +0000

    Rebuild check_guess function

--- run.py
+++ run.py
@@ -10,6 +10,10 @@ def get_word():
     word = random.choice(word_list)
     return word.upper()
 
+
+word = get_word()
+
+
 # menu, play / instructions (rules)
 # play again?
 # already guessed
@@ -33,123 +37,132 @@ def greeting():
 # greeting()
 
 
-def game_mode():
-    mode = input("Choose game mode (EASY/HARD): ")
-    max_fails = stages
-    if mode != "EASY" and mode != "HARD":
-        print("Invalid input, try EASY/HARD")
-    elif mode == "easy":
-        max_fails = stages
-        print("You chose EASY, you get 6 attempts")
-    else:
-        max_fails = stages[1:-1:2]
-        print("You chose HARD, you get 3 attempts")
-    return mode.upper()
+# def game_mode():
+#     mode = input("Choose game mode (EASY/HARD): ")
+#     max_fails = stages
+#     if mode != "EASY" and mode != "HARD":
+#         print("Invalid input, try EASY/HARD")
+#     elif mode == "easy":
+#         max_fails = stages
+#         print("You chose EASY, you get 6 attempts")
+#     else:
+#         max_fails = stages[1:-1:2]
+#         print("You chose HARD, you get 3 attempts")
+#     return mode.upper()
 
 
-def check_guess():
+def check_guess(word):
     """
     Check whether player`s input is a letter or not
     """
+    guessed_letters = []
+    fails = 0
     while True:
-        guess = input("Guess a letter: ")
-        guess = guess.upper()
+        guess = input("Guess a letter: ").upper()
         if guess.isalpha() and len(guess) == 1:
-            print(f"{guess} is Valid")
+            if guess in guessed_letters:
+                print(f"You already guessed: {guess} ")
+            elif guess not in word:
+                print(f"{guess} is not in the word.")
+                fails += 1
+                guessed_letters.append(guess)
         else:
             print(f"{guess} Invalid. Try one letter")
 
 
-# check_guess()
+check_guess(word)
 
 
-# Change function to array of stages so max_fails
-# can get a different number of stages by indexing
-
-stages = [
+def display_hangman(fails):
     """
-    +---+
-    |   |
-    |
-    |
-    |
-    ______
-    """,
+    Display the different stages of hangman based of number of failures
     """
-    +---+
-    |   |
-    |   O
-    |
-    |
-    ______
-    """,
-    """
-    +---+
-    |   |
-    |   O
-    |   |
-    |
-    ______
-    """,
-    """
-    +---+
-    |   |
-    |   O
-    |  /|
-    |
-    ______
-    """,
-    """
-    +---+
-    |   |
-    |   O
-    |  /|\\
-    |
-    ______
-    """,
-    """
-    +---+
-    |   |
-    |   O
-    |  /|\\
-    |  /
-    ______
-    """,
-    """
-    +---+
-    |   |
-    |   O
-    |  /|\\
-    |  / \\
-    ______
-    """
-]
-
-
-def get_mode():
-    """
-    Prompt the user to choose game mode
-    """
-    global stages
-    try:
-        difficulty = input("Choose game mode (EASY/HARD): ")
-        difficulty = difficulty.upper()
-        if difficulty == 'easy':
-            print("You chose EASY")
-            return stages
-        elif difficulty == 'hard':
-            print("You chose HARD")
-            stages = stages[1:-1:2]
-            return stages
-        else:
-            return stages
-    except ValueError():
-        if difficulty != "easy" and difficulty != "hard":
-            print("Invalid input, choose EASY/HARD")
-    return
+    stages = [
+        """
+        +---+
+        |   |
+        |
+        |
+        |
+        ______
+        """,
+        """
+        +---+
+        |   |
+        |   O
+        |
+        |
+        ______
+        """,
+        """
+        +---+
+        |   |
+        |   O
+        |   |
+        |
+        ______
+        """,
+        """
+        +---+
+        |   |
+        |   O
+        |  /|
+        |
+        ______
+        """,
+        """
+        +---+
+        |   |
+        |   O
+        |  /|\\
+        |
+        ______
+        """,
+        """
+        +---+
+        |   |
+        |   O
+        |  /|\\
+        |  /
+        ______
+        """,
+        """
+        +---+
+        |   |
+        |   O
+        |  /|\\
+        |  / \\
+        ______
+        """
+    ]
+    return stages[fails]
 
 
-get_mode()
+# def get_mode():
+#     """
+#     Prompt the user to choose game mode
+#     """
+#     global stages
+#     while True:
+#         try:
+#             mode = input("Choose game mode (EASY/HARD): ")
+#             mode = mode.upper()
+#             if mode == 'easy':
+#                 print("You chose EASY")
+#                 return stages
+#             elif mode == 'hard':
+#                 print("You chose HARD")
+#                 stages = stages[1:-1:2]
+#                 return stages
+#             else:
+#                 return stages
+#         except ValueError():
+#             if mode != "easy" and mode != "hard":
+#                 print("Invalid input, choose EASY/HARD")
+#         return
+
+
+# get_mode()
 
 
 # while attempt <= 6:
@@ -157,66 +170,3 @@ get_mode()
 #     print("The word is:")
 #     hidden_word = "_" * len(word)
 #     print(hidden_word)
-
-
-# def display_hangman(attempt):
-#     """
-#     Display the different stages of hangman based of number of failures
-#     """
-#     if attempt == 0:
-#         print("""
-#             +---+
-#             |   |
-#             |
-#             |
-#             |
-#             ______""")
-#     elif attempt == 1:
-#         print("""
-#             +---+
-#             |   |
-#             |   O
-#             |
-#             |
-#             ______""")
-#     elif attempt == 2:
-#         print("""
-#             +---+
-#             |   |
-#             |   O
-#             |   |
-#             |
-#             ______""")  
-#     elif attempt == 3:
-#         print("""
-#             +---+
-#             |   |
-#             |   O
-#             |  /|
-#             |
-#             ______""")
-#     elif attempt == 4:
-#         print("""
-#             +---+
-#             |   |
-#             |   O
-#             |  /|\\
-#             |
-#             ______""")
-#     elif attempt == 5:
-#         print("""
-#             +---+
-#             |   |
-#             |   O
-#             |  /|\\
-#             |  /
-#             ______""")
-#     elif attempt == 6:
-#         print("""
-#             +---+
-#             |   |
-#             |   O
-#             |  /|\\
-#             |  / \\
-#             ______""")
-#     return attempt
